a88c27e9056b855b1fcb693d867a1c7d00c93c44
Justin Bailey <jgbailey@codeslower.com>
2011-07-18 14:09:31 -0700

    Revisions and updates.

--- draggable.js
+++ draggable.js
@@ -4,35 +4,35 @@
     items: [{
       xtype: 'panel',
       title: 'Drop a Panel Here',
+      listeners: { render: initializeDropTarget },
+      cls: 'x-dd-drop-ok',
       layout: {
         type: 'vbox',
         stretch: 'align'
       },
       width: 300,
-      height: 300,
-      cls: 'x-dd-drop-ok',
-      listeners: { render: initializeDropTarget }
+      height: 300
     }, {
       xtype: 'panel',
       title: 'Drag This Panel',
+      draggable: true,
       bodyCls: 'green-body',
       width: 300,
-      height: 100,
-      draggable: true
+      height: 100
     }, {
       xtype: 'panel',
       title: 'Drag This Panel',
+      draggable: true,
       bodyCls: 'blue-body',
       width: 300,
-      height: 100,
-      draggable: true
+      height: 100
     }, {
       xtype: 'panel',
       title: 'Drag This Panel',
+      draggable: true,
       bodyCls: 'red-body',
       width: 300,
-      height: 100,
-      draggable: true
+      height: 100
     }]
   });
 });
@@ -42,42 +42,21 @@ Initialize the DropTarget object associated
 with our panel. The 'cmp' argument will be
 the panel (a Component object).
 */
-function initializeDropTarget(cmp) {
+function initializeDropTarget(targetPanel) {
   // Create the DropTarget object and assign it to the panel. Does not
   // have to be assigned to the panel but needs to be assigned to something,
   // or it will get garbage-collected too soon.
-  cmp.dropTarget = Ext.create('Ext.dd.DropTarget', cmp.el);
-
-  // Called once, when dragged item enters drop area.
-  cmp.dropTarget.notifyEnter = function(source, evt, data) {
-    if(typeof console != "undefined")
-      console.log("notifyEnter:" + source.id);
-    return this.callParent(Array.prototype.slice.call(arguments));
-  };
-
-  // Called once, when dragged item leaves drop area.
-  cmp.dropTarget.notifyOut = function(source, evt, data) {
-    if(typeof console != "undefined")
-      console.log("notifyOut:" + source.id);
-    return this.callParent(Array.prototype.slice.call(arguments));
-  };
-
-  // Called for each mouse movement as dragged item is over the drop area.
-  cmp.dropTarget.notifyOver = function(source, evt, data) {
-    if(typeof console != "undefined")
-      console.log("notifyOver:" + source.id);
-    return this.callParent(Array.prototype.slice.call(arguments));
-  };
+  targetPanel.dropTarget = Ext.create('Ext.dd.DropTarget', targetPanel.el);
 
   // Called once, when dragged item is dropped in the target area. Return false
   // to indicate an invalid drop. DO NOT MODIFY the UI in 
   // this function. Use afterDragDrop and the data object.
-  cmp.dropTarget.notifyDrop = function(source, evt, data) {
+  targetPanel.dropTarget.notifyDrop = function(source, evt, data) {
     if(typeof console != "undefined")
       console.log("notifyDrop:" + source.id);
 
     // The component that was dropped.
-    var droppedCmp = Ext.getCmp(source.id);
+    var droppedPanel = Ext.getCmp(source.id);
 
     // We can't modify the component that was dropped in this
     // function. However, we can add an event handler on the component
@@ -85,15 +64,40 @@ function initializeDropTarget(cmp) {
     //
     // In the handler we clone the component (not strictly necessary, we could
     // do that here) and then remove our old component.  
-    droppedCmp.dd.afterValidDrop = function() {
-      cmp.add(droppedCmp.cloneConfig({
+    droppedPanel.dd.afterValidDrop = function() {
+      targetPanel.add(droppedPanel.cloneConfig({
         draggable: false,
         title: "Can't Drag This Panel."
       }));
 
-      droppedCmp.destroy();
+      droppedPanel.destroy();
     };
 
     return true;
   };
+
+  // Called once, when dragged item enters drop area.
+  targetPanel.dropTarget.notifyEnter = function(source, evt, data) {
+    if(typeof console != "undefined")
+      console.log("notifyEnter:" + source.id);
+
+    return this.callParent(Array.prototype.slice.call(arguments));
+  };
+
+  // Called once, when dragged item leaves drop area.
+  targetPanel.dropTarget.notifyOut = function(source, evt, data) {
+    if(typeof console != "undefined")
+      console.log("notifyOut:" + source.id);
+
+    return this.callParent(Array.prototype.slice.call(arguments));
+  };
+
+  // Called for each mouse movement as dragged item is over the drop area.
+  targetPanel.dropTarget.notifyOver = function(source, evt, data) {
+    if(typeof console != "undefined")
+      console.log("notifyOver:" + source.id);
+
+    return this.callParent(Array.prototype.slice.call(arguments));
+  };
+
 }
